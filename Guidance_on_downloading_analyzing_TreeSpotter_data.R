64b541bbc57d4bc011b9c456549d6df772e07968
alinazeng <alinazengziyun@yahoo.com>
2021-05-11 17:13:35 -0700

    creating a script for downloading data

---
+++ Guidance_on_downloading_analyzing_TreeSpotter_data.R
@@ -0,0 +1,6 @@
+# Hello Tree Spotters & fellow enthusiasts!
+# This script was created to provide you some guidance on 
+        # downloading Tree Spotter
+        # tidying up the data so you can perform some simple analysis as you wish
+
+
