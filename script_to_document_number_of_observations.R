bce9d203d31c2260dfaad945e05f5058ee098e86
alinazeng <alinazengziyun@yahoo.com>
2021-05-13 12:08:44 -0700

    added links to resources

--- script_to_document_number_of_observations.R
+++ script_to_document_number_of_observations.R
@@ -4,4 +4,17 @@
 # alinazeng(at)ubc.ca
 
 
+# can represent some info using pie charts 
+# eg. out of 300000 records, 40% (120000 were made on XXX route)
+# http://data.usanpn.org/npn-viz-tool/
+
+
+# also need to work on mapping using GIS
+# need to download coordinates from this site: https://arboretum.harvard.edu/explorer/
+# clean/format csv. files and then map
+# http://map.arboretum.harvard.edu/arcgis/rest/services/CollectionExplorer/MapServer
+
+
+
+
 
