b71ae539b6e6c35c1e778a73fcde71b3d7d511ee
alinazeng <83845945+alinazeng@users.noreply.github.com>
2021-05-13 12:09:49 -0700

    Update script_to_document_number_of_observations.R

--- script_to_document_number_of_observations.R
+++ script_to_document_number_of_observations.R
@@ -1,4 +1,4 @@
-# This script will be use to figure out which routes/species/individuals were most frequently observed
+# This script will be used to figure out which routes/species/individuals were most frequently observed
 # Development is underway
 # May-13-2021
 # alinazeng(at)ubc.ca
