8b8bdd66854acb67a906ef7e3e9ce5298892bab6
alinazeng <alinazengziyun@yahoo.com>
2021-05-27 11:45:05 -0700

    created new folder

--- analyses/examples_cat/exampleplots.R
+++ analyses/examples_cat/exampleplots.R
@@ -20,28 +20,29 @@ leaves <- subset(df, select=c(scientific_names, year, bb_mean, leafout_mean))
 leaves <- gather(leaves, pheno, doy, -scientific_names, -year)
 # minus sign means leaving scientific_names and year out of the gathering process
 
-quartz() # alina: not sure what this is about
+quartz() # 
 # cols <- viridis_pal(option="plasma")(15) ### example on how to use viridis package
 colz <- c("salmon3", "royalblue3")
 leaves$code <- reorder(leaves$scientific_names, leaves$doy) ## this reorders the species by day of budburst
-# hmmm not super sure^^^
+
 leaves$colz <-ifelse(leaves$pheno=="bb_mean", "salmon3", "royalblue3")
 
 # calculate year 2016 ----
+#  pdf(res = 350)
 leaves2016 <- ggplot(leaves[(leaves$year==2016),], aes(x=scientific_names, y=doy)) + geom_point(aes(color=rev(colz), shape=pheno)) +
   geom_line(col="green4", alpha=0.5) + ylab("Day of Year") + 
   theme(panel.background = element_blank(), axis.line = element_line(colour = "black"),
                                                                   axis.text.y = element_text(face = "italic"),
                                                                   axis.text=element_text(size=9), legend.key = element_rect(fill = "transparent"),
-                                                                  legend.box.background = element_rect(),
+                                                                  # legend.box.background = element_rect(),
                                                                   panel.spacing = unit(2, "lines"),
                                                                   plot.title = element_text(color="#1B9E77"),
-                                                                  legend.position = "none",
+                                                                  # legend.position = "none",
                                                                   axis.title.y = element_blank()) + labs(col="Phenophase") + 
   scale_shape_manual(name="Phenophase", values=c(16, 17), labels=c("Budburst", "Leafout")) +
   scale_color_manual(name="Phenophase", values=c("salmon3", "royalblue3"), labels=c("Budburst", "Leafout")) + ggtitle("2016") +
   coord_flip() 
-
+dev.off()
 leaves2016
 
 ### Looks like something funny might be going on with the oaks... I would check out your calculations of means
@@ -64,6 +65,18 @@ leaves2017 <- ggplot(leaves[(leaves$year==2017),], aes(x=scientific_names, y=doy
   scale_shape_manual(name="Phenophase", values=c(16, 17), labels=c("Budburst", "Leafout")) +
   scale_color_manual(name="Phenophase", values=c("salmon3", "royalblue3"), labels=c("Budburst", "Leafout")) + ggtitle("2017") +
   coord_flip() 
+
+# theme_classic.minimal
+# theme_classic() +
+# theme_minimal() +
+# theme(legend.position = “none”) 
+# arrange(leaves$scientific_names)
+# scale_x_manual(breaks=seq(100, 140, by=10))
+# scale_x_discrete()
+# ave()
+# geom_smooth()
+# library(lubridate)
+# yday(df$date)
 
 
 # calculate year 2018 ----
