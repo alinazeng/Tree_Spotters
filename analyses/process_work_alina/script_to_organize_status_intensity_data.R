ae460eb1403868c8c18bf4200c3625afaf94ba8b
alinazeng <alinazengziyun@yahoo.com>
2021-06-02 13:29:50 -0700

    recalculated numbers of observation using raw data

--- analyses/process_work_alina/script_to_organize_status_intensity_data.R
+++ analyses/process_work_alina/script_to_organize_status_intensity_data.R
@@ -86,14 +86,106 @@ observer_year <- rename(observer_year, observation_year = observation,
 
 
 # export 
-write.csv(occurence, file = "output/occurence_of_observation_status_June_2", row.names = F)
-write.csv(observation_year, file = "output/number_of_observation_each_year_June_2", row.names = F)
-write.csv(observer_year, file = "output/number_of_observation_by_each_individual_each_year_June_2", row.names = F)
-
-
+write.csv(occurence, file = "output/occurence_of_observation_status_June_2.csv", row.names = F)
+write.csv(observation_year, file = "output/number_of_observation_each_year_June_2.csv", row.names = F)
+write.csv(observer_year, file = "output/number_of_observation_by_each_individual_each_year_June_2.csv", row.names = F)
 
 
 # get rid of Os and -1s and recalculate # of observations
 
+pheno_cleaned <- subset(bb.pheno,bb.pheno$Phenophase_Status == "1")
+# 66963 obs
+
+# use this to calculate numbers of observation ----
+# combine two systems of naming
 
 
+# separate plant nicknames into two columns
+library(tidyr)
+pheno_cleaned <- pheno_cleaned %>% 
+  tidyr::separate(Plant_Nickname, c("Plant_ID", "Species_Nickname"), sep = ", ", 
+                  remove = FALSE) 
+
+treeinfo <- read.csv("input/MyVisit_filtered.csv", header = TRUE)
+
+# rename columns and filter out unwanted ones
+treeinfo <- rename(treeinfo,Plant_ID = Plant.ID,
+                   Latitude = Garden.Latitude,
+                   Longitude = Garden.Longitude,
+                   "DBH(cm)" = DBH)
+
+# joining data frames by Plant_ID
+d <- full_join(pheno_cleaned, treeinfo)
+
+
+
+# count the number of observations made along each route ----
+route_obs <- d %>%
+  group_by(route) %>%
+  summarise("route_obs#" = length(route))
+
+# make a table with species name, coordinates, # of observation, individual ID
+d <- full_join(d,route_obs)
+
+# count the number of observations made on individual trees and shrubs ----
+indiv_obs  <- d %>%
+  group_by(id) %>%
+  summarise("indiv_obs#" = length(id))
+
+# count the number of observations made on each species
+spp_obs <- d %>%
+  group_by(Common_Name) %>%
+  summarise("spp_obs#" = length(Common_Name))
+
+# count the number of observations made on each phenophase
+pheno_obs <- d %>%
+  group_by(phase) %>%
+  summarise("pheno_obs#" = length(phase))
+
+######################################################################
+# update May-26, 2021 ----
+# count the number of observations of each phenophase made on each species 
+pheno_spp_obs <- d %>%
+  group_by(phase,Common_Name) %>%
+  summarise("pheno_spp_obs#" = length(phase))
+
+# count the number of observations of each phenophase made on each individual 
+pheno_indiv_obs <- d %>%
+  group_by(phase,id) %>%
+  summarise("pheno_indiv_obs#" = length(phase))
+
+# quickly linking Individual ID to scientific names
+pheno_indiv_obs <- full_join(pheno_indiv_obs,dplyr::select(d, c(Common_Name, id))) %>%  unique()
+
+# update on June-2, 2021
+# counting using status intensity data
+write.csv(pheno_spp_obs,file = "output/observation_pheno_spp_update_June_2.csv",row.names=FALSE)
+write.csv(pheno_indiv_obs,file = "output/observation_pheno_indiv_update_June_2.csv",row.names=FALSE)
+
+
+
+d <- full_join(d,indiv_obs)
+d <- full_join(d,spp_obs)
+d <- full_join(d,pheno_obs)
+d <- full_join(d,pheno_spp_obs)
+d <- full_join(d,pheno_indiv_obs)
+
+
+# join the genus and the species columns ----
+d$Scientific_Names <- with(d, paste(genus, species, sep = " "))
+
+# add in scientific names into spp_obs and indiv_obs ----
+scientific_names <- dplyr::select(d, c(Scientific_Names, Common_Name, id))
+scientific_names <- unique(scientific_names)
+
+indiv_obs <- full_join(scientific_names,indiv_obs)
+spp_obs <- full_join((scientific_names %>% 
+                        dplyr::select(-id) %>% 
+                        unique()),spp_obs)
+
+# export ----
+write.csv(d,file = "output/observation_table_all_June_2.csv",row.names=FALSE)
+write.csv(indiv_obs,file = "output/observation_individual_trees_June_2.csv",row.names=FALSE)
+write.csv(spp_obs,file = "output/observation_species_June_2.csv",row.names=FALSE)
+write.csv(pheno_obs,file = "output/observation_pheno_June_2.csv",row.names=FALSE)
+write.csv(route_obs,file = "output/observation_routes_June_2.csv",row.names=FALSE)
